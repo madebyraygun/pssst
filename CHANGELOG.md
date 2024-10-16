5fe8c3ec0007b4b7be04ddce1602b96baf3852fa
Dalton Rooney <dalton@madebyraygun.com>
2024-10-16 12:23:39 -0700

    feat: turnstile uses default color theme

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## Unreleased
+
+- Dynamic theme for Turnstile widget. 
+
 ## 0.1.0 - 2024-10-16
 
 - Initial public release.
--- src/web/assets/app.js
+++ src/web/assets/app.js
@@ -1,7 +1,15 @@
+const cfTsWidget = document.querySelector('.cf-turnstile');
+
 if (localStorage.getItem('theme')) {
   document.documentElement.setAttribute('data-theme', localStorage.getItem('theme'))
+  if (cfTsWidget) {
+    cfTsWidget.setAttribute('data-theme', localStorage.getItem('theme'))
+  }
 } else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
   document.documentElement.setAttribute('data-theme', 'dark')
+  if (cfTsWidget) {
+    cfTsWidget.setAttribute('data-theme', 'dark')
+  }
 }
 
 let currentTheme = document.documentElement.getAttribute('data-theme') || (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light')
@@ -9,5 +17,8 @@ const contrast = document.querySelector('.contrast')
 contrast.addEventListener('click', function() {
   currentTheme = currentTheme === 'dark' ? 'light' : 'dark'
   document.documentElement.setAttribute('data-theme', currentTheme)
+  if (cfTsWidget) {
+    cfTsWidget.setAttribute('data-theme', currentTheme)
+  }
   localStorage.setItem('theme', currentTheme)
 })
